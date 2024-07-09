4f7a0749212ce2aa584a70ab64de8438fb2e66dc
mvpclash <roy3r91@gmail.com>
2024-07-09 08:29:07 -0600

    Creación de estilo para formulario

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(()=>{
+   const denunciasBtn = document.getElementById('denuncias');
+   const usuarioHtml = document.getElementById('usuario');
+   const passwordHtml = document.getElementById('password');
+    
+    denunciasBtn.addEventListener('click', (evento) =>{
+       
+        
+    })
+    
+
+
+
+
+}
+
+)();
